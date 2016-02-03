538148b4fd89d1fb39e85d251ed367bc9953f0a3
Jenna Bantjes <jenna.bantjes@freshtilledsoil.com>
2016-02-03 16:29:18 -0500

    Add contact form functionality

---
+++ SalterTrans/contactthanks.php
@@ -0,0 +1,49 @@
+<!DOCTYPE HTML PUBLIC "-//W3C//DTD HTML 4.0 Strict//EN">
+
+<head>
+	<meta http-equiv="Content-Type" content="text/html; charset=UTF-8" />
+	<link rel="stylesheet" type="text/css" href="styles/styles.css">
+	<link href='https://fonts.googleapis.com/css?family=Open+Sans:400,300,300italic,400italic' rel='stylesheet' type='text/css'>	
+	<title>Salter Transportation</title>
+</head>
+
+<body>
+
+	<header class="page-wrapper">
+		<div class="logo">
+			<a href="index.html"><img src="images/logo.png" /></a>
+		</div>
+		
+		<input type="checkbox" id="menu">
+		<label for="menu" id="menu-label" onclick>Menu </label>
+		
+		<nav class="main-menu">
+			<div id="resources">
+				<ul>
+					<a href="parent-resources.html"><li>Parent Resources</li></a>
+					<a href="about.html"><li>About Us</li></a>
+					<a href="quote.html" class="quote"><li>Request a Quote</li></a>
+				</ul>
+			</div>
+			<div id="services">
+				<ul>
+					<a href="schools.html"><li>Schools</li></a>
+					<a href="weddings.html"><li>Weddings</li></a>
+					<a href="charters.html"><li>Charter</li></a>
+					<a href="careers.html"><li>Careers</li></a>
+				</ul>
+			</div>
+		</nav>
+	</header>
+
+	<main class="page-wrapper">
+		
+		<h1>Your message has been sent!</h1><br />
+		
+		<p><a href="index.html">Back to Homepage</a></p>
+	
+	</main>
+
+</body>
+
+</html>
